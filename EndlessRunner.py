1d9606687ddfb6f4e70724a4a8b560eaa645b497
Sophia <44215812+SophFru@users.noreply.github.com>
2021-02-27 18:54:56 -0600

    Update EndlessRunner.py

--- EndlessRunner.py
+++ EndlessRunner.py
@@ -1,10 +1,14 @@
 import pygame, sys
 from pygame.locals import *
 
+#Set up window
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300)) #TODO: Change to monitor size
 pygame.display.set_caption('Hello World')
-while True: #main game loop
+DISPLAYSURF.fill((0, 0, 100)) #Navy Blue
+
+#Main Game Loop
+while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
