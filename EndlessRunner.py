fc7c03e47ce12b49a4cb4481a7db22a46b77b1f3
Sophia <44215812+SophFru@users.noreply.github.com>
2021-02-27 18:39:07 -0600

    Update EndlessRunner.py
    Fixed typo on line 11

--- EndlessRunner.py
+++ EndlessRunner.py
@@ -2,11 +2,11 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+DISPLAYSURF = pygame.display.set_mode((400, 300)) #TODO: Change to monitor size
 pygame.display.set_caption('Hello World')
 while True: #main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.ext()
+            sys.exit()
     pygame.display.update()
